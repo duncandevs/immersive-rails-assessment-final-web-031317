bac1799af5e732c7bda89daf2fe814c2c28b5702
github username <github email address>
2017-04-21 10:42:59 -0400

    Done.

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -1,4 +1,5 @@
 class AppearancesController < ApplicationController
+  before_action :require_login , only:[:new, :edit]
 
   def new
     @appearance = Appearance.new
@@ -13,10 +14,30 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(rating: params[:appearance][:rating], user_id: params[:appearance][:user_id])
+    if @appearance.valid?
+      redirect_to @appearance.episode
+    else
+      redirect_to edit_appearance_path
+    end
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
+  end
+
+  def require_login
+    unless logged_in?
+      redirect_to login_path
+    end
   end
 
 end
